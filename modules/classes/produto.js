c88d0f6933c6f29bf0dc5ff7fecff526c6d52881
ZanellaIgor <igor_zanella@outlook.com>
2023-08-29 00:40:46 -0300

    first

---
+++ modules/classes/produto.js
@@ -0,0 +1,9 @@
+export class Produto {
+    constructor(nome, unidade, referencia, estoque, imagem) {
+        this.nome = nome;
+        this.unidade = unidade;
+        this.referencia = referencia;
+        this.estoque = estoque;
+        this.imagem = imagem;
+    }
+}
